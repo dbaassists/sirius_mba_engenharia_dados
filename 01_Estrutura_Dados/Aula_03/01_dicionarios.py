efb654007b6ab130b1406c786c373b2fa2fa7680
Gabriel Quintella <dbaassits@gmail.com>
2024-09-12 19:13:43 -0300

    Arquivos da aula 03 de estrutura de dados

---
+++ 01_Estrutura_Dados/Aula_03/01_dicionarios.py
@@ -0,0 +1,13 @@
+#########################################################################
+# Aula 03 - Estrutura de Dados
+# Autor: Gabriel Quintella
+# Data: 12/09/2024
+# Assunto: 1. DICIONARIOS (Dict)
+# Um dicionário é uma coleção de pares chave-valor, onde as chaves são únicas 
+# e os valores podem ser de qualquer tipo. Ele é uma estrutura de dados mutável.
+#########################################################################
+
+
+# %%
+
+
