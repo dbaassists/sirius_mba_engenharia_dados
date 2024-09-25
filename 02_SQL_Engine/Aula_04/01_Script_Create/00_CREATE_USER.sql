6fc151ab67d8e303fbfc3bd9bc0db141c076c6a9
Gabriel Quintella <dbaassits@gmail.com>
2024-09-24 22:37:14 -0300

    Upload arquivos de SQL Engines - Aula 04

---
+++ 02_SQL_Engine/Aula_04/01_Script_Create/00_CREATE_USER.sql
@@ -0,0 +1,60 @@
+CREATE LOGIN [rodrigo.charles] 
+WITH PASSWORD=N'RodCha(äsd86Q!2024>'
+GO
+CREATE USER [rodrigo.charles] FOR LOGIN [rodrigo.charles]
+GO
+
+CREATE LOGIN [nathan.arrais] 
+WITH PASSWORD=N'NatArr(äsd86Q!2024>'
+GO
+CREATE USER [nathan.arrais] FOR LOGIN [nathan.arrais]
+GO
+
+
+CREATE LOGIN [flavia.castro] 
+WITH PASSWORD=N'FlaCas(äsd86Q!2024>'
+GO
+CREATE USER [flavia.castro] FOR LOGIN [flavia.castro]
+GO
+
+
+CREATE LOGIN [wesley.mical] 
+WITH PASSWORD=N'WesMic(äsd86Q!2024>'
+GO
+CREATE USER [wesley.mical] FOR LOGIN [wesley.mical]
+GO
+
+CREATE LOGIN [marielle.miziara] 
+WITH PASSWORD=N'MarMiz(äsd86Q!2024>'
+GO
+CREATE USER [marielle.miziara] FOR LOGIN [marielle.miziara]
+GO
+
+
+
+
+server: srv-mba-engenharia-dados.database.windows.net
+database: db-mba-engenharia-dados
+username: rodrigo.charles
+password: RodCha(äsd86Q!2024>
+
+server: srv-mba-engenharia-dados.database.windows.net
+database: db-mba-engenharia-dados
+username: nathan.arrais
+password: NatArr(äsd86Q!2024>
+
+server: srv-mba-engenharia-dados.database.windows.net
+database: db-mba-engenharia-dados
+username: flavia.castro
+password: FlaCas(äsd86Q!2024>
+
+server: srv-mba-engenharia-dados.database.windows.net
+database: db-mba-engenharia-dados
+username: wesley.mical
+password: WesMic(äsd86Q!2024>
+
+server: srv-mba-engenharia-dados.database.windows.net
+database: db-mba-engenharia-dados
+username: marielle.miziara
+password: MarMiz(äsd86Q!2024>
+
